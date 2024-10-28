282829e56085a14df6dd4158d5f16047be018bcf
YSShalini <125855192+YSShalini@users.noreply.github.com>
2024-10-28 23:13:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
@@ -8,18 +8,19 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/news-aggregator">
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/homepage" element={<Homepage />} />
-        </Routes>
-      </div>
-    </Router>
+    <Router>
+  <div className="App">
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/homepage" element={<Homepage />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  </div>
+</Router>
+
   );
 }
-
 
 export default App;
